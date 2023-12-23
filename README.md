2eb43c8ad4734f3edbde25f31016fef7f550155b
JM Lima <jml@jmlima.com>
2023-12-23 11:59:47 -0600

    Deleted all test data, replaced with last 24 hours hourly data.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Auction_House
 Analysis of WoW Auction House Prices
+
+
+
+get new data steps:
+terminal: aws s3 sync s3://{bucket-name} ./raw-data
+run: add_timestamp.py
+check with: data_to_df_example.ipynb
+run: get_unique_item_ids.py
---
+++ get_unique_item_ids.py
@@ -0,0 +1,59 @@
+import json
+import os
+
+def process_json_files(folder_path):
+    unique_item_ids = set()
+    processed_files = set()
+
+    # Check if './unique_item_ids.json' exists and load its contents if it does
+    if os.path.exists('unique_item_ids.json'):
+        with open('unique_item_ids.json', 'r') as file:
+            previous_data = json.load(file)
+            unique_item_ids = set(previous_data['unique_item_ids'])
+            processed_files = set(previous_data['processed_files'])
+
+    initial_unique_ids_count = len(unique_item_ids)
+    files_processed = False  # Flag to check if any new file was processed
+
+    # Traverse through all files in the folder
+    for filename in os.listdir(folder_path):
+        if filename.endswith('.json') and filename not in processed_files:
+            files_processed = True  # Mark that a new file is processed
+            file_path = os.path.join(folder_path, filename)
+
+            # Process each JSON file
+            with open(file_path, 'r') as file:
+                data = json.load(file)
+                auctions = data.get('auctions', [])
+
+                # Extract unique item IDs
+                for auction in auctions:
+                    item_id = auction.get('item', {}).get('id')
+                    if item_id is not None:
+                        unique_item_ids.add(item_id)
+
+                # Add filename to processed files
+                processed_files.add(filename)
+
+    # Write to item-names.json
+    output_data = {
+        'unique_item_ids': list(unique_item_ids),
+        'processed_files': list(processed_files)
+    }
+    with open('unique_item_ids.json', 'w') as output_file:
+        json.dump(output_data, output_file, indent=4)
+
+    # Print the number of new IDs found and the total number of unique item IDs
+    new_ids_found = len(unique_item_ids) - initial_unique_ids_count
+    total_unique_ids = len(unique_item_ids)
+    print(f"New IDs found: {new_ids_found}")
+    print(f"Total unique item IDs: {total_unique_ids}")
+
+    # Print message if no new files were processed
+    if not files_processed:
+        print("All files in the data folder have been processed.")
+
+    print("Data written to unique-item-ids.json")
+
+# Replace 'data' with the path to your data folder
+process_json_files('data')
